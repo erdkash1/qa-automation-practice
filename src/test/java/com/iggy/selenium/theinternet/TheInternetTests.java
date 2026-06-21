835c984f98a1966a3279b92889ff0943a10b3669
Erdkash1 <erdkash1@gmail.com>
2026-06-20 23:55:03 -0500

    Add Changes

---
+++ src/test/java/com/iggy/selenium/theinternet/TheInternetTests.java
@@ -0,0 +1,50 @@
+package com.iggy.selenium.theinternet;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TheInternetTests {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private static final String BASE_URL = "https://the-internet.herokuapp.com";
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void shouldLoadHomePageSuccessfully() {
+        driver.get(BASE_URL);
+
+        String title = driver.getTitle();
+        assertEquals("The Internet", title,
+                "Page title should be 'The Internet'");
+    }
+}
