61367d68316623dce4f36b8ae07332c5569e72c5
Erdkash1 <erdkash1@gmail.com>
2026-06-23 23:55:14 -0500

    implement Page Object Model for login tests with proper wait handling

--- src/test/java/com/iggy/selenium/theinternet/LoginPage.java
+++ src/test/java/com/iggy/selenium/theinternet/LoginPage.java
@@ -2,13 +2,19 @@ package com.iggy.selenium.theinternet;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 public class LoginPage {
 
     private WebDriver driver;
+    private WebDriverWait wait;
+
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
     private By usernameField = By.id("username");
     private By passwordField = By.id("password");
@@ -27,7 +33,7 @@ public class LoginPage {
         driver.findElement(loginButton).click();
     }
 
-    public String getFlashMessageText(){
-        return driver.findElement(flashMessage).getText();
+    public String getFlashMessageText() {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(flashMessage)).getText();
     }
 }
