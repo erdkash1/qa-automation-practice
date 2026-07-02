e249444e9d80231bdea2cedf9e218b8d5d0a8623
Erdkash1 <erdkash1@gmail.com>
2026-07-01 23:28:07 -0500

    test: add SauceDemo products page tests with login flow

---
+++ src/test/java/com/iggy/selenium/saucedemo/SauceDemoProductsTests.java
@@ -0,0 +1,62 @@
+package com.iggy.selenium.saucedemo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SauceDemoProductsTests {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private SauceDemoLoginPage sauceDemoLoginPage;
+    private SauceDemoProductsPage sauceDemoProductsPage;
+    private static final String BASE_URL = "https://www.saucedemo.com";
+
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get(BASE_URL);
+        sauceDemoLoginPage = new SauceDemoLoginPage(driver);
+        sauceDemoLoginPage.enterUsername("standard_user");
+        sauceDemoLoginPage.enterPassword("secret_sauce");
+        sauceDemoLoginPage.clickLoginButton();
+        sauceDemoProductsPage = new SauceDemoProductsPage(driver);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void shouldDisplayProductsTitle(){
+        assertEquals("Products", sauceDemoProductsPage.getPageTitle());
+
+    }
+
+    @Test
+    void shouldDisplaySixProducts(){
+        assertEquals(6, sauceDemoProductsPage.getProductCount());
+    }
+
+    @Test
+    void shouldDisplayFirstProduct(){
+        assertEquals("Sauce Labs Backpack", sauceDemoProductsPage.getFirstProductName());
+    }
+}
