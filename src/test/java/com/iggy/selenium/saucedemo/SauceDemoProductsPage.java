f80c62b7f04f053861e8b58e5dff8f72ddd83fc9
Erdkash1 <erdkash1@gmail.com>
2026-07-02 22:01:59 -0500

    add cart badge tests - verify count after adding items

--- src/test/java/com/iggy/selenium/saucedemo/SauceDemoProductsPage.java
+++ src/test/java/com/iggy/selenium/saucedemo/SauceDemoProductsPage.java
@@ -25,6 +25,10 @@ public class SauceDemoProductsPage {
     private By itemNames = By.cssSelector("[data-test='inventory-item-name']");
     private By itemPrices = By.cssSelector("[data-test='inventory-item-price']");
 
+    private By addToCartBackpack = By.cssSelector("[data-test='add-to-cart-sauce-labs-backpack']");
+    private By addToCartBikeLight = By.cssSelector("[data-test='add-to-cart-sauce-labs-bike-light']");
+    private By cartBadge = By.cssSelector("[data-test='shopping-cart-badge']");
+
     public String getPageTitle(){
         return wait.until(ExpectedConditions.presenceOfElementLocated(pageTitle)).getText();
     }
@@ -37,5 +41,19 @@ public class SauceDemoProductsPage {
     public String getFirstProductName(){
         return driver.findElements(itemNames).get(0).getText();
     }
+
+    public void addBackpackToCart(){
+        driver.findElement(addToCartBackpack).click();
+    }
+
+    public void addBikeLightToCart(){
+        driver.findElement(addToCartBikeLight).click();
+    }
+
+    public String getCartBadgeCount(){
+        return wait.until(ExpectedConditions.presenceOfElementLocated(cartBadge)).getText();
+    }
+
+
 
 }
