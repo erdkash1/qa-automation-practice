fb3c28778ea95f6ed1bf6e3ba159232f95d739b3
Erdkash1 <erdkash1@gmail.com>
2026-07-06 22:59:06 -0500

    add Allure Reports with Epic/Feature/Story annotations and flaky test management

--- src/test/java/com/iggy/selenium/theinternet/iframe/IFramePage.java
+++ src/test/java/com/iggy/selenium/theinternet/iframe/IFramePage.java
@@ -3,6 +3,7 @@ package com.iggy.selenium.theinternet.iframe;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -18,16 +19,16 @@ public class IFramePage {
     private By iframe = By.id("mce_0_ifr");
     private By editorBody = By.id("tinymce");
 
-    public void switchToIFrame(){
-        driver.switchTo().frame(driver.findElement(iframe));
+    public void switchToIFrame() {
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(iframe));
     }
     public void switchToMainContent(){
         driver.switchTo().defaultContent();
     }
-    public String getEditorText(){
+    public String getEditorText() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(editorBody));
         return driver.findElement(editorBody).getText();
     }
-
     public void clearEditorText() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].innerHTML = '';", driver.findElement(editorBody));
@@ -37,4 +38,5 @@ public class IFramePage {
         js.executeScript("arguments[0].innerHTML = arguments[1];",
                 driver.findElement(editorBody), text);
     }
+
 }
