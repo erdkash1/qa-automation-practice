159bee89c5db70bf9e2079ad89d288d7103a521c
Erdkash1 <erdkash1@gmail.com>
2026-07-11 23:07:57 -0500

    add Cucumber BDD tests for SauceDemo login scenarios

---
+++ src/test/java/com/iggy/cucumber/steps/LoginSteps.java
@@ -0,0 +1,65 @@
+package com.iggy.cucumber.steps;
+
+import com.iggy.selenium.saucedemo.SauceDemoLoginPage;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginSteps {
+
+    private WebDriver driver;
+    private SauceDemoLoginPage loginPage;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+
+    @Given("I am on the SauceDemo login page")
+    public void iAmOnTheSauceDemoLoginPage() {
+        driver.get("https://www.saucedemo.com");
+        loginPage = new SauceDemoLoginPage(driver);
+    }
+
+    @When("I enter username {string}")
+    public void iEnterUsername(String username) {
+        loginPage.enterUsername(username);
+    }
+
+    @And("I enter password {string}")
+    public void iEnterPassword(String password) {
+        loginPage.enterPassword(password);
+    }
+
+    @And("I click the login button")
+    public void iClickTheLoginButton() {
+        loginPage.clickLoginButton();
+    }
+
+    @Then("I should be redirected to the inventory page")
+    public void iShouldBeRedirectedToTheInventoryPage() {
+        assertTrue(driver.getCurrentUrl().contains("inventory"));
+    }
+
+    @Then("I should see an error message containing {string}")
+    public void iShouldSeeAnErrorMessageContaining(String expectedError) {
+        assertTrue(loginPage.getErrorMessage().contains(expectedError));
+    }
+}
