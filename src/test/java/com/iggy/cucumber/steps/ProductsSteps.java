7e48d5de711f6f787167f279f07cd20a544782dd
Erdkash1 <erdkash1@gmail.com>
2026-07-13 22:15:31 -0500

    add Cucumber cart feature with shared context using PicoContainer

--- src/test/java/com/iggy/cucumber/steps/ProductsSteps.java
+++ src/test/java/com/iggy/cucumber/steps/ProductsSteps.java
@@ -6,7 +6,6 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -14,8 +13,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductsSteps {
 
-    private WebDriver driver;
-    private SauceDemoProductsPage productsPage;
+    private final SharedContext context;
+
+    public ProductsSteps(SharedContext context) {
+        this.context = context;
+    }
 
     @Before
     public void setUp() {
@@ -25,46 +27,46 @@ public class ProductsSteps {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
-        driver = new ChromeDriver(options);
+        context.driver = new ChromeDriver(options);
     }
 
     @After
     public void tearDown() {
-        if (driver != null) driver.quit();
+        if (context.driver != null) context.driver.quit();
     }
 
     @Given("I am logged in as {string} with password {string}")
     public void iAmLoggedInAs(String username, String password) {
-        driver.get("https://www.saucedemo.com");
-        SauceDemoLoginPage loginPage = new SauceDemoLoginPage(driver);
+        context.driver.get("https://www.saucedemo.com");
+        SauceDemoLoginPage loginPage = new SauceDemoLoginPage(context.driver);
         loginPage.enterUsername(username);
         loginPage.enterPassword(password);
         loginPage.clickLoginButton();
-        productsPage = new SauceDemoProductsPage(driver);
+        context.productsPage = new SauceDemoProductsPage(context.driver);
     }
 
     @Then("I should see the page title {string}")
     public void iShouldSeeThePageTitle(String expectedTitle) {
-        assertEquals(expectedTitle, productsPage.getPageTitle());
+        assertEquals(expectedTitle, context.productsPage.getPageTitle());
     }
 
     @Then("I should see {int} products")
     public void iShouldSeeProducts(int expectedCount) {
-        assertEquals(expectedCount, productsPage.getProductCount());
+        assertEquals(expectedCount, context.productsPage.getProductCount());
     }
 
     @Then("the first product should be {string}")
     public void theFirstProductShouldBe(String expectedName) {
-        assertEquals(expectedName, productsPage.getFirstProductName());
+        assertEquals(expectedName, context.productsPage.getFirstProductName());
     }
 
     @When("I sort products by {string}")
     public void iSortProductsBy(String sortOption) {
-        productsPage.sortByPriceLowToHigh();
+        context.productsPage.sortByPriceLowToHigh();
     }
 
     @Then("the first product price should be {string}")
     public void theFirstProductPriceShouldBe(String expectedPrice) {
-        assertEquals(expectedPrice, productsPage.getFirstProductPrice());
+        assertEquals(expectedPrice, context.productsPage.getFirstProductPrice());
     }
 }
